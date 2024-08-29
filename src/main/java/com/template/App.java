a6ef13be44c29e4e1e29db3d2c40e46a0e7e2f8e
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-08-28 23:22:27 -0300

    Update App.java

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -7,7 +7,35 @@ public class App {
 
 
     public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
-        return Collections.emptyMap();
+        if(accounts != null && accounts.containsKey(account)) {
+            int new_amount = accounts.get(account) + amount;
+            if(amount < 0) { return accounts; }
+            accounts.replace(account, new_amount);
+
+        }
+        return accounts;
+    }
+
+    public static Map<String, Integer> withdraw(Map<String, Integer> accounts, String account, int amount) {
+     
+        if(accounts != null &&accounts.containsKey(account)) {
+            if(amount <= 0 || accounts.get(account) < amount ) { return accounts; }
+            
+            int new_amount = accounts.get(account) - amount;
+            accounts.replace(account, new_amount);
+
+        }
+        return accounts;
+    }
+
+    public static Map<String, Integer> transfer(Map<String, Integer> accounts, String user_1, int amount, String user_2) {
+        
+        if(accounts == null || !(accounts.containsKey(user_1)) || !(accounts.containsKey(user_2)) || accounts.get(user_1) < amount || amount < 0) { return accounts; }
+
+        accounts.replace(user_1, accounts.get(user_1) - amount);
+        accounts.replace(user_2, accounts.get(user_2) + amount);
+
+        return accounts;
     }
 
 
